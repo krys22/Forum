e64da4a214676bff14dbe3f3158711c29688b089
Chris <c.uzor@yahoo.com>
2020-12-13 00:27:05 +0100

    Authentication Scaffold Added

--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -7,6 +7,12 @@ use Faker\Generator as Faker;
 
 $factory->define(Reply::class, function (Faker $faker) {
     return [
-        //
+        'user_id' => function(){
+            return factory('App\User')->create()->id;
+        },
+        'thread_id' => function(){
+            return factory('App\Thread')->create()->id;
+        },
+        'body' => $faker->paragraph,
     ];
 });
