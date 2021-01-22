8f83451cb692942dba629dbbba23bacf7d9c2b59
Chris <c.uzor@yahoo.com>
2021-01-22 01:22:30 +0100

    User owns replies

---
+++ resources/views/threads/reply.blade.php
@@ -0,0 +1,11 @@
+<div class="card">
+    <div class="card-header">
+    <a> {{$reply->owner->name}} </a> said   {{ $reply->created_at->diffForHumans() }}
+    </div>
+      <div class="card-body">
+                <article>
+                    <div class="body"> {{$reply->body}} </div>
+                </article>
+                <hr>
+    </div>
+</div>
